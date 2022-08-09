8c492801cadf864b2373b7ef73187400c47f958b
ansoni01 <40308661+ansoni01@users.noreply.github.com>
2022-08-09 02:26:57 -0500

    Inicio de proyecto

---
+++ src/main/java/com/retoback/RetobackApplication.java
@@ -0,0 +1,13 @@
+package com.retoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetobackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retoback/RetobackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retoback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetobackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
